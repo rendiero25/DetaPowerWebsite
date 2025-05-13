11981a8b0408afad33c1956e2b899eb441cd91b9
Rendy Tomaluweng <workspace.rendy@gmail.com>
2025-05-13 23:40:34 +0700

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Detapower Website</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
